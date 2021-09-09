f1a577355b828a3e05deb1b2c38548d9063088b5
Kristopher C. Castillo <kccastillo2@Kristophers-MacBook-Air-2.local>
2021-09-09 18:35:46 -0400

    Removed hello/world test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,10 +6,6 @@ const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
 
-router.get("/hello/world", function (req, res) {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
 
 
 
