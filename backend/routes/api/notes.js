b7b8429fb3a60581df19c9717a8181e8074e47be
Kristopher C. Castillo <kccastillo2@Kristophers-MacBook-Air-2.local>
2021-09-13 19:18:14 -0400

    SContinued building home component

--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -1,6 +1,22 @@
 const express = require("express");
 
+const asyncHandler = require("express-async-handler");
+
+
 const router = express.Router();
+
+const { Note } = require("../../db/models");
+
+router.get(
+  "/",
+  asyncHandler(async function (req, res) {
+    const notes = await Note.findAll();
+
+    return res.json({
+      notes
+    });
+  })
+);
 
 
 module.exports = router;
