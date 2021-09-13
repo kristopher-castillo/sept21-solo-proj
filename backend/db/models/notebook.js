d5e9eedc518eacac186d645c449e2f9086488318
Kristopher C. Castillo <kccastillo2@Kristophers-MacBook-Air-2.local>
2021-09-13 13:25:37 -0400

    Added Note and Notebook models/migrations, began working on associations

---
+++ backend/db/models/notebook.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Notebook = sequelize.define("Notebook", {
+      title: {
+        type: DataTypes.STRING(120),
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {}
+  );
+  Notebook.associate = function(models) {
+    // associations can be defined here
+  };
+  return Notebook;
+};
