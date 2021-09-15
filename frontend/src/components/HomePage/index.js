dbe0cf3a0c7199aaa5955da67e861d9e07b67eff
Kristopher C. Castillo <kccastillo2@Kristophers-MacBook-Air-2.local>
2021-09-15 18:23:58 -0400

    Styling note page

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { getNotes } from "../../store/notes";
+
+const HomePage = () => {
+
+  return (
+    <div>
+      <p>This is the HomePage</p>
+    </div>
+  )
+
+}
+
+export default HomePage;
