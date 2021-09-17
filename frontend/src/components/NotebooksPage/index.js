21b995f5f294beb62a32ec4863df96e0790bd989
Kristopher C. Castillo <kccastillo2@Kristophers-MacBook-Air-2.local>
2021-09-16 22:36:00 -0400

    Continued building notebooks component

--- frontend/src/components/NotebooksPage/index.js
+++ frontend/src/components/NotebooksPage/index.js
@@ -1,39 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getNotes,  createNewNote, updateOneNote, updateNoteTitle, deleteOneNote} from "../../store/notes";
+import { getNotebooks, createNewNotebook, updateNotebookTitle, updateOneNotebook, deleteOneNotebook} from "../../store/notebooks";
 import { Redirect, NavLink, useHistory } from "react-router-dom";
 import "./NotebooksPage.css"
 
 const NotebooksPage = () => {
   const sessionUser = useSelector((state) => state.session.user);
-  const allNotes = useSelector((state) => state.notes.notes);
-  const notes = allNotes.filter((note) => note.userId === sessionUser?.id);
-  const allNotebooks = useSelector((state) => state.notebooks.notebooks);
-  const notebooks = allNotebooks.filter((notebook) => notebook.userId === sessionUser?.id);
-  
-  
+  // const allNotes = useSelector((state) => state.notes.notes);
+  // const notes = allNotes.filter((note) => note.userId === sessionUser?.id);
+  const allNotebooks = useSelector((state) => state.notebooks);
+  console.log(allNotebooks)
+  const notebooks = allNotebooks.filter((notebook) => notebook?.userId === sessionUser?.id);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(getNotebooks());
+    }, [dispatch]);
 
   return (
     <>
       <div>
-        <p>This is the Notebooks Page!</p>
         <div className="notebooksPageContainer">
+          <h2>Your Notebooks</h2>
           <div className="notebooksBar">
-            <ul>
-              {notes.map((note) => (
-                <li key={note.id} onClick={() => selectedNoteAction(note)}>
-                  <NavLink to={`/notes/${note.id}`}>{note.title}</NavLink>
+            {/* <ul>
+              {notebooks.map((notebook) => (
+                <li key={notebook.id}>
+                  <NavLink to={`/notebooks/${notebook.id}`}>{notebook.title}</NavLink>
                 </li>
               ))}
-            </ul>
+            </ul> */}
             <button
-              type="button"
-              onClick={() => {
-                setShowForm("");
-                setShowNote("none");
-                history.push("/notes");
-              }}>
-              Add a Note
+              type="button">
+              Add a Notebook
             </button>
           </div>
         </div>
